b68694b1b24df61ee1229bead2c642ebecd3b357
amshal1 <panshal911@gmail.com>
2023-10-20 19:38:58 +0530

    blinking animation and dynamic cursor added

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 class TypeAnim {
-  constructor({ wordslist, typing_speed, show_cursor, element, wait_time }) {
+  constructor({
+    wordslist,
+    typing_speed,
+    show_cursor,
+    element,
+    wait_time,
+    cursor_type,
+  }) {
     // arrays of words you wanna show
     this.words = wordslist;
     // speed of the typing of words
@@ -11,14 +18,26 @@ class TypeAnim {
     this.element = element;
     // how long should we wait after we finish writing one word
     this.wait_time = wait_time || 1000;
+    // what type of cursor you wanna show while writing
+    this.cursor_type = cursor_type || "|";
     // index of the current word
     this.index = 0;
     this.write();
+    this.drawCursor(this.cursor_type);
   }
-  drawCursor() {
+  drawCursor(text) {
+    if (!this.cursor) return;
     const cursor = document.createElement("span");
-    cursor.textContent = "|";
-    this.element.append(cursor);
+    cursor.setAttribute("data-cursor", "true");
+    cursor.textContent = text;
+    this.element.parentNode.append(cursor);
+    const FadeKeyframse = new KeyframeEffect(
+      cursor,
+      [{ opacity: "0" }, { opacity: "1" }],
+      { duration: this.wait_time, iterations: Infinity, direction: "alternate" }
+    );
+    const fade_anim = new Animation(FadeKeyframse, document.timeline);
+    fade_anim.play();
   }
   wait(time) {
     return new Promise((resolve) => {
@@ -30,13 +49,12 @@ class TypeAnim {
       const current_word = this.words[this.index];
       for (let i = 0; i < current_word.length; i++) {
         this.element.innerText = current_word.substring(0, i + 1);
-        this.drawCursor();
         await this.wait(this.speed);
       }
       await this.wait(this.wait_time);
       for (let i = current_word.length; i > 0; i--) {
+        await this.wait(50);
         this.element.innerText = current_word.substring(0, i - 1);
-        this.drawCursor();
         await this.wait(this.speed);
       }
       await this.wait(this.wait_time);
